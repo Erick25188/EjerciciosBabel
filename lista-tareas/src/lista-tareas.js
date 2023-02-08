0806509c24726a120cfe844d0287fcd6da2cdac2
Erick Manzano <erickmanzanoipn@gmail.com>
2023-02-08 11:38:11 -0600

    terminado, faltan estilos

--- lista-tareas/src/lista-tareas.js
+++ lista-tareas/src/lista-tareas.js
@@ -1,4 +1,4 @@
-import {LitElement, html, nothing} from 'lit-element';
+import {LitElement, html, nothing, css} from 'lit-element';
 
 class ListaTareas extends LitElement {
 
@@ -12,6 +12,16 @@ class ListaTareas extends LitElement {
         campoVacio : { type: Boolean  },
         hideCompleted: { type: Boolean}
     }
+  }
+
+  static get styles() {
+    return css`
+    #root
+    .bg-gray{
+      background-color: red;
+      color:red
+    }
+    `
   }
 
   constructor() {
@@ -75,31 +85,37 @@ class ListaTareas extends LitElement {
   render() {
 
     return html`
-    <div class="container-md">
+  <div class="bg-primary text-white">
+    <div class="container-md pt-5 pb-5">
+      <h1 class="h1">Lista de tareas.</h1>
       <form @submit=${this._submit}>
         <input
+          class="form-control mb-3"
           id="inputTarea"
           type="text"
           name="tarea"
           placeholder="agrega una tarea"
         >
-        <button type="submit">Agregar</button>
-        <input type="checkbox"  @change=${this.setHideCompleted}
-        ?checked=${this.hideCompleted}>
+        <button style="background-color: var(--bs-primary-bg-subtle);--bs-border-color:var(--bs-primary-border-subtle);color: var(--bs-primary-text);" class="btn  btn-lg" type="submit">Agregar</button>
+          <div class="float-end" >
+          <label><strong>Ocultar tareas realizadas</strong></label>
+          <input type="checkbox"  @change=${this.setHideCompleted}
+          ?checked=${this.hideCompleted}>
+        </div>
       </form>
-
-       
+    </div>
 
       ${ this.textoValidar }
-
-     
-      <div class="row justify-content-center">
+      <hr class="border border-3 opacity-75" style="background-color: var(--bs-primary-bg-subtle);">
+  </div>
+  <div class="container mb-5">
+      <div class="row justify-content-center mt-5 " >
         <div class="col-md-8 ">
             <ul class="list-group ">
             ${
               this.listaTareas.map( (tarea,index) => html`
                 <li
-                  class="list-group-item d-flex align-items-center ${this.hideCompleted && tarea.completado ? 'd-none' : tarea.completado ? 'bg-info-subtle' : 'd-block'}
+                  class="list-group-item d-flex align-items-center ${this.hideCompleted && tarea.completado ? 'd-none' : tarea.completado ? 'bg-gray' : 'd-block'}
                                                                     ">
                     <input class="form-check-input me-1" type="checkbox" ?checked=${tarea.completado} @change="${()=>this.onchange(index)}"> 
                     <label
@@ -107,7 +123,7 @@ class ListaTareas extends LitElement {
                       for="firstCheckbox"
                       class="${tarea.completado ? 'text-decoration-line-through fw-bold ' : nothing}"
                       > ${tarea.tarea}</label>
-                    <button class="btn btn-primary ms-auto" @click=${() => this._eliminar(index)}>Eliminar</button> 
+                    <button class="btn btn-danger ms-auto" @click=${() => this._eliminar(index)}>Eliminar</button> 
                 </li>
               `)
             }
